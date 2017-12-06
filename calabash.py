e606cbbdf2a71657f726fe78af2aa1b91c07067a
ShuaiyiHuang <huangshuaiyi2013@163.com>
2017-12-06 21:04:01 +0800

    begin with bug

--- calabash.py
+++ calabash.py
@@ -1,7 +1,9 @@
+import numpy as np
+import time
+from numpy import unravel_index
 def power_by_mtt(state, edges):
     """Calculate the total power of the state, by the matrix-tree theorem.
     """
-    import numpy as np
 
     n = len(state)
     graph = np.zeros((n+1, n+1), dtype=np.float64)
@@ -36,6 +38,118 @@ def randomized_algorithm():
             best_power = power
     assert best_state is not None
     print ' '.join('%+d' % i for i in best_state)
+    print power
+
+def greedy_algorithm():
+    import random
+    random.seed(0)
+
+    n, edges = read_input()
+    times = 1
+    best_state, best_power = None, None
+
+    startt=time.time()
+    #for each starting store max weight
+    pos_topos,pos_toneg,neg_topos,neg_toneg=4*[-1*np.ones((n+1,n+1),dtype=float)]
+
+    for edge in edges:
+        u,v,w=edge
+        indu=abs(u)
+        indv=abs(v)
+        assert (type(u)==int and type(v)==int and type(w)==float)
+        if u>0 and v>0:
+            u, v = abs(u), abs(v)
+            pos_topos[u,v]=w
+        if u>0 and v<0:
+            u, v = abs(u), abs(v)
+            pos_toneg[u,v]=w
+        if u<0 and v>0:
+            u, v = abs(u), abs(v)
+            neg_topos[u,v]=w
+        if u<0 and v<0:
+            print('neg to nge:',u,v)
+            u, v = abs(u), abs(v)
+            neg_toneg[u,v]=w
+        if u==0:
+            if v>0:
+                print('before 0:',neg_toneg[indu,indv])
+                print('v>0:',u,v,w)
+                neg_topos[u,v]=w
+                pos_topos[u,v]=w
+                print('after 0:',neg_toneg[indu,indv])
+            if v<0:
+                print('v<0:',u,v,w)
+                neg_toneg[u,abs(v)]=w
+                pos_toneg[u,abs(v)]=w
+    endt=time.time()
+    assert (neg_topos[0,:].all()==pos_topos[0,:].all())
+    assert (pos_toneg[0,:].all()==neg_toneg[0,:].all())
+    print('index finished in {}'.format((endt-startt)))
+
+    print ('neg to neg\n',neg_toneg)
+    print('neg to pos:\n', neg_topos)
+    print('pos to neg:/n', pos_toneg)
+    print('pos to pos:' + '/n', pos_topos)
+
+    mystate=[]
+
+    matrix=np.concatenate([np.expand_dims(neg_toneg,0),np.expand_dims(neg_topos,0),np.expand_dims(pos_toneg,0),np.expand_dims(pos_topos,0)],0)
+    print(matrix[0].shape)
+    print(matrix[1].shape)
+    print(matrix[3])
+
+    nodeid_prev=0
+
+    print('preliminary matrix:',matrix)
+    for i in range(1,n+1):
+        nodeid=-99
+
+        fromneg=[0,1]
+        frompos=[2,3]
+        fromsign=[]
+        if nodeid_prev<0:
+            fromsign=fromneg
+        else:
+            fromsign=frompos
+
+        vec = matrix[fromsign, nodeid_prev, :]
+        print('vec shape:',vec.shape)
+        print('vec:',vec)
+
+        maxw=np.max(vec)
+        indice=unravel_index(vec.argmax(), vec.shape)
+        sign, nodeindex = indice
+        assert (len(indice) == 2)
+        print('max indice:',sign,nodeindex)
+        print('maxw:',maxw)
+
+        if(sign==0):
+            #newly find nodeid is neg
+            nodeid=-(abs(nodeindex))
+        elif(sign==1):
+            #newly find nodeid is pos
+            nodeid=abs(nodeindex)
+
+        #disable chosen id,set to negative
+        print('before set to neg:',matrix[:,:,nodeindex])
+        matrix[:,:,nodeindex]=-1
+        matrix[:, nodeid_prev, :] = -1
+        print('after set to neg:',matrix[:,:,nodeindex])
+        print('noid:',nodeid,'matrix:',matrix)
+        mystate.append(nodeid)
+        nodeid_prev=nodeid
+    print('mystate:',mystate)
+    state=tuple(mystate)
+
+    for _ in range(times):
+        # state = tuple(i * (-1)**random.randrange(1, 3) for i in range(1, n+1))
+        power = power_by_mtt(state, edges)
+        if best_state is None or best_power < power:
+            best_state = state
+            best_power = power
+    assert best_state is not None
+    print ' '.join('%+d' % i for i in best_state)
+    print power
 
 
 def read_input():
@@ -49,4 +163,5 @@ def read_input():
 
 
 if __name__ == '__main__':
-    randomized_algorithm()
+    #randomized_algorithm()
+    greedy_algorithm()
